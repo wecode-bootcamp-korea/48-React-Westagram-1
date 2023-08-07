b356abac891bf33e4935cd27d1ad316f8f0ea0f3
serenity1091 <seulki1091@gmail.com>
2023-08-07 16:30:55 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/kimseulki/Login/Login.js
+++ src/pages/kimseulki/Login/Login.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import "../../../style/reset.scss";
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; 
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const goToMain = () => {
+    navigate('/kim-main')
+  }
+
+  const saveUserId = event => {
+    setId(event.target.value);
+  };
+
+  const saveUserPw = event => {
+    setPw(event.target.value);
+  };
+
   return (
     <div className="login">
       <h1 className="title_hd">로그인페이지</h1>
@@ -12,9 +30,11 @@ const Login = () => {
           <p className="login_font logo_font">westagram</p>
         </div>
         <div className="login_inputWrap">
-          <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일"/>
-          <input type="password" placeholder="비밀번호"></input>
-          <Link className="login_link" to='/kim-main'><button>로그인</button></Link>
+          <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" id="id"
+            value={id} onChange={saveUserId}/>
+          <input type="password" placeholder="비밀번호" id="pw"
+            value={pw} onChange={saveUserPw}></input>
+          <button className="login_link" onClick={goToMain}>로그인</button>
         </div>
         <div className="login_lost link">
           <Link to='#!'><p>비밀번호를 잊으셨나요?</p></Link>
